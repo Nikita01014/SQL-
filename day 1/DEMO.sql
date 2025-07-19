cbaa3aec474b64d8f34518dcdb54ad01666b9b83
NIKITA VISHWAKARMA <ns04012001@gmail.com>
2025-07-19 09:55:07 +0530

    Add files via upload

---
+++ day 1/DEMO.sql
@@ -0,0 +1,41 @@
+-- single line comment
+
+/*
+multiline 
+comments
+*/
+
+## this is also used as a single line comments
+
+-- -------------------------------------- Database Queries ---------------------------------------------------
+
+-- create a database first
+create database DemoSchool;
+
+-- to work on a database you need to use it fist
+use DemoSchool;
+-- create new database 
+
+/*
+Usage of comments
+
+1. Documentation: Cpmments can be used to document the purpose oa a SQL query or the logic
+behind compex opreations.
+2. Debugging: you can temporarily disable parts of your SQL code by commenting them out,
+which is useful for debuggung.
+3. Collaboration: When working in teams, comments help communicate the intent and 
+functionality of the code to other developers.
+*/
+
+-- -------------------------------------- Database Queries ---------------------------------------------
+
+-- Create a database first
+Create database SKP;
+
+-- to work on this database, you need to use it first
+-- to execute ( ctrl + enter)
+use skp;
+ 
+ -- SQL is not a case-sensitive language (As- AGE or age will be treated as same word)
+ 
+ -- -------------------------------------------- Table Related Queries -------------------------------
